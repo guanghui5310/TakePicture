e3fed87ede202867c23f511c6ff853e16f567e78
jihymood <huangjihy@163.com>
2017-12-05 15:52:22 +0800

    增加几个单图片上传和多图片上传方法

--- app/src/main/java/com/cc/csdndemo1/MainActivity.java
+++ app/src/main/java/com/cc/csdndemo1/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -21,6 +22,7 @@ import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
@@ -30,6 +32,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,12 +40,15 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.takephotoTV)
     Button takephotoTV;
-    @BindView(R.id.savephotoTV)
-    Button savephotoTV;
+    @BindView(R.id.saveTxt)
+    Button saveTxt;
     @BindView(R.id.imageIV)
     ImageView imageIV;
+    @BindView(R.id.savePhoto)
+    Button savePhoto;
 
     private final int CAMERA_REQUEST = 8888;
+    Bitmap bitmap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,15 +58,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @OnClick({R.id.takephotoTV, R.id.savephotoTV})
+    @OnClick({R.id.takephotoTV, R.id.saveTxt, R.id.savePhoto})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.takephotoTV:
                 Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
                 break;
-            case R.id.savephotoTV:
+            case R.id.saveTxt:
                 getFile();
+                break;
+            case R.id.savePhoto:
+                uploadPictureOne(bitmap);
                 break;
         }
     }
@@ -68,24 +77,23 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Bitmap bitmap = null;
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
             bitmap = (Bitmap) data.getExtras().get("data");
             imageIV.setImageBitmap(bitmap);
+
+//            //设置第二张图片
+//            image1.setDrawingCacheEnabled(true);
+//            if (image1.getDrawingCache() != null) {
+//                image2.setImageBitmap(bitmap);
+//            }
         }
-
-
-        uploadPicture(bitmap);
-
-
-
     }
 
 
     /**
      * 上传图片
      */
-    public void uploadPicture(Bitmap bitmap) {
+    public void uploadPictureOne(Bitmap bitmap) {
         /**
          * Map<String, InputStream>
          * 方法一
@@ -101,6 +109,10 @@ public class MainActivity extends AppCompatActivity {
          * Map<String, RequestBody>
          * 方法二：map里的key必须是file.getName() + "\"; filename=\"" + file.getName()这样
          * 这样才能在后台的方法中接收到，具体是为什么有待研究
+         * 前面一个file1.getName()是为了传递给后台作为文件名使用的，
+         * 在writeToLocal()方法里的第二行 String dirFile = destDirName + fileName;
+         * 但是呢尽量还是使用后面的file.getName()，后台在选择的文件名的时候尽量选择value
+         * 参考下面单文件上传方法(1)注释
          */
         Map<String, RequestBody> map = new HashMap<>();
         File file1 = compressImage(bitmap);
@@ -109,20 +121,78 @@ public class MainActivity extends AppCompatActivity {
         map.put(file1.getName() + "\"; filename=\"" + file1.getName(), requestBody);
 
 
+        /**
+         * 单文件上传方法(1)
+         * 一：如果后台选用的是eachKey传入方法writeToLocal中
+         * 那么这里的"uploadFile"会作为图片名被使用，
+         * 如果后台没有规定.jpg格式的那么这里就需要加上
+         * 二：如果后台选用了fileName作为文件名使用，那么这里的uploadFile就可以随便写什么了
+         */
+        File file2 = compressImage(bitmap);
+        //图片参数
+        RequestBody imageBody = RequestBody.create(MediaType.parse("multipart/form-data"), file2);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("uploadFile", file2.getName(), imageBody);
+        //手机号参数
+        RequestBody phoneBody = RequestBody.create(MediaType.parse("multipart/form-data"), "18896751425");
+        //密码参数
+        RequestBody pswBody = RequestBody.create(MediaType.parse("multipart/form-data"), "jjj");
+
+
+        /**
+         * 单文件上传方法(2)
+         * 参考单文件上传方法(1)的注释
+         */
+        File file3 = compressImage(bitmap);
+        RequestBody imageBody1 = RequestBody.create(MediaType.parse("multipart/form-data"), file3);
+        MultipartBody.Builder builder = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("phone", "18896751425")
+                .addFormDataPart("password", "jjj")
+                .addFormDataPart("uploadFile", file3.getName(), imageBody1);
+        List<MultipartBody.Part> parts = builder.build().parts();
+
+
+        /**
+         * 多文件传方法(1)
+         */
+        File file4 = compressImage(bitmap);
+        RequestBody requestFile1 = RequestBody.create(MediaType.parse("multipart/form-data"), file4);
+        MultipartBody.Part body1 = MultipartBody.Part
+                .createFormData("uploadFile", file4.getName(), requestFile1);
+        RequestBody requestFile2 = RequestBody.create(MediaType.parse("multipart/form-data"), file4);
+        MultipartBody.Part body2 = MultipartBody.Part
+                .createFormData("uploadFile", file4.getName(), requestFile2);
+
+
+        /**
+         * 多文件上传方法(2)
+         * 采用map集合来存放多个图片RequestBody参数。
+         * 将多文件传方法(1)的代码放到map中
+         */
+        Map<String, RequestBody> map1 = new HashMap<>();
+        map1.put("文件1" + "\"; filename=\"" + "hh.jpg", requestFile1);
+        map1.put("文件2" + "\"; filename=\"" + "shabi.jpg", requestFile2);
+
+
         ApiManager.getInstance()
                 .pointApiService()
 //                .uploadPicture1(files)
 //                .uploadPicture2(map)
-//                .uploadPicture(map)
-                .uploadPicture3("photo1111", map)
+//                .uploadPicture("photo", map)
+//                .uploadPicture3("photo1111", map)
+//                .uploadPictureOne(phoneBody, pswBody, body)
+//                .uploadPictureOne(parts)
+                .uploadPictures("pictures", requestFile1, requestFile2)
+//                .uploadPictures("pictures", map1)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new io.reactivex.functions.Consumer<ResponseObj<Boolean>>() {
+                .subscribe(new Consumer<ResponseObj<Boolean>>() {
                     @Override
                     public void accept(ResponseObj<Boolean> booleanResponseObj) throws Exception {
+                        Toast.makeText(MainActivity.this, "上传成功", Toast.LENGTH_SHORT).show();
                         Log.e("MainActivity", "booleanResponseObj:" + booleanResponseObj.data.toString());
                     }
-                }, new io.reactivex.functions.Consumer<Throwable>() {
+                }, new Consumer<Throwable>() {
                     @Override
                     public void accept(Throwable throwable) throws Exception {
                         Log.e("MainActivity", "throwable:" + throwable);
@@ -184,7 +254,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
 
 
     /**
--- app/src/main/java/com/cc/csdndemo1/PointApi.java
+++ app/src/main/java/com/cc/csdndemo1/PointApi.java
@@ -2,9 +2,11 @@ package com.cc.csdndemo1;
 
 
 import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 
 import io.reactivex.Observable;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
@@ -17,30 +19,100 @@ import retrofit2.http.PartMap;
 
 public interface PointApi {
 
-    @Multipart
-    @POST("takePicture/uploadPicture")
-    Observable<ResponseObj<Boolean>> uploadPicture1(@PartMap Map<String, InputStream> map);
+//    @Multipart
+//    @POST("takePicture/uploadPicture")
+//    Observable<ResponseObj<Boolean>> uploadPicture1(@PartMap Map<String, InputStream> map);
+//
+//    @Multipart
+//    @POST("takePicture/uploadPicture")
+//    Observable<ResponseObj<Boolean>> uploadPicture2(@PartMap Map<String, RequestBody> map);
 
-    @Multipart
-    @POST("takePicture/uploadPicture")
-    Observable<ResponseObj<Boolean>> uploadPicture2(@PartMap Map<String, RequestBody> map);
-
-
+    /**
+     * 将图片保存到本地电脑
+     *
+     * @param map
+     * @return
+     */
     @Multipart
     @POST("takePicture/picture")
-    Observable<ResponseObj<Boolean>> uploadPicture(@PartMap Map<String, RequestBody> map);
+    Observable<ResponseObj<Boolean>> uploadPicture(@Part("photo") String photo,
+                                                   @PartMap Map<String, RequestBody> map);
 
 
+    /**
+     * 单文件上传方法(1)
+     * 注册时接口有三个参数：手机号、密码、头像
+     *
+     * @param phone
+     * @param password
+     * @param image
+     * @return
+     */
+    @Multipart
+    @POST("takePicture/picture")
+    Observable<ResponseObj<Boolean>> uploadPictureOne(@Part("phone") RequestBody phone,
+                                                         @Part("password") RequestBody password,
+                                                         @Part MultipartBody.Part image);
+
+    /**
+     * 单文件上传方法(2)
+     * 注册时接口有三个参数：手机号、密码、头像
+     * 可以发现方法中的参数变成了List《MultipartBody.Part》的集合。这样所有的参数我们只需要放到这个集合里边即可
+     *
+     * @return
+     */
+    @Multipart
+    @POST("takePicture/picture")
+    Observable<ResponseObj<Boolean>> uploadPictureOne(@Part List<MultipartBody.Part> partList);
+
+
+    /**
+     * 多文件上传方法(1)
+     */
+    @Multipart
+    @POST("takePicture/picture")
+    Observable<ResponseObj<Boolean>> uploadPictures(@Part("filename") String description,
+                                                    @Part("pic\"; filename=\"image1.png") RequestBody imgs1,
+                                                    @Part("pic\"; filename=\"image2.png") RequestBody imgs2);
+
+    /**
+     * 多文件上传方法(2)
+     * 采用map集合来存放多个图片RequestBody参数。
+     */
+    @Multipart
+    @POST("takePicture/picture")
+    Observable<ResponseObj<Boolean>> uploadPictures(@Part("filename") String description,
+                                                    @PartMap() Map<String, RequestBody> maps);
+
+    /**
+     * 上传FTP服务器，参考内蒙运维zhywserver项目中装机工单详情里的回单接口
+     *
+     * @param photo
+     * @param map
+     * @return
+     */
     @Multipart
     @POST("takePicture/uploadPicture")
     Observable<ResponseObj<Boolean>> uploadPicture3(@Part("photo") String photo,
-                                                @PartMap Map<String, RequestBody> map);
+                                                    @PartMap Map<String, RequestBody> map);
 
 
+    /**
+     * 上传文件.txt
+     *
+     * @param map
+     * @return
+     */
     @Multipart
     @POST("takePicture/uploadFile")
     Observable<ResponseObj<Boolean>> uploadFile(@PartMap Map<String, InputStream> map);
 
+    /**
+     * 上传文件.txt
+     *
+     * @param map
+     * @return
+     */
     @Multipart
     @POST("takePicture/uploadFile")
     Observable<ResponseObj<Boolean>> uploadFile1(@PartMap Map<String, RequestBody> map);
